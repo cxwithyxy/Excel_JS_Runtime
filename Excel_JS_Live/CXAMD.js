ced61779f493e66e92b49d8e1e3fd0f8d8aa2539
chenxuan <523764720@qq.com>
2018-03-19 13:34:18 +0800

    define_Eaze： 初步找到解决方案

--- Excel_JS_Live/CXAMD.js
+++ Excel_JS_Live/CXAMD.js
@@ -7,6 +7,8 @@ CXAMD.net_Mode = (function ()
     return ThisWorkbook.Name.indexOf(".NET_MODE.") != -1;
 })();
 
+CXAMD.
+
 define = function (_moduleList, _f){
     var moduleLoadedInThisTime = [];
     for(i in _moduleList){
@@ -24,5 +26,41 @@ define = function (_moduleList, _f){
         moduleLoadedInThisTime.push(CXAMD.allModuleLoaded[theFilePath]);
     }
     return _f.apply(this, moduleLoadedInThisTime);
+};
+
+deine_Eaze = function (_moduleList, _f){
+    var moduleLoadedInThisTime = [];
+    for(i in _moduleList){
+        var theFilePath = _moduleList[i];
+        if(typeof CXAMD.allModuleLoaded[theFilePath] == typeof UDFUDF){
+            var jsFileData = "";
+            if (CXAMD.net_Mode) {
+                jsFileData = getJSInNetwork(CXAMD.net_Url, theFilePath);;
+            }
+            if(jsFileData.length < 1){
+                jsFileData = getJSInExcel(theFilePath);
+            }
+            CXAMD.allModuleLoaded[theFilePath] = eval(jsFileData);
+        }
+        moduleLoadedInThisTime.push(CXAMD.allModuleLoaded[theFilePath]);
+    }
+    function inVar(inObj, funS)
+    {
+        return eval(
+            "(function (){"+
+
+                "return function (a){" +
+                    "with(a){"+
+                        funS +
+                    "}"+
+                "}"+
+            "})();"
+        )(inObj);
+    }
+    function get_Func_Content(fg)
+    {
+        return fg.toString().split("{").reverse()[0].split("}")[0];
+    }
+    // return _f.apply(this, moduleLoadedInThisTime);
 };
 define.amd = true;
